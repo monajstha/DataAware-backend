c9381d4a5b9447bdd4ef006677e093980d68a808
monajstha <manojstha700@gmail.com>
2025-12-15 10:01:31 +0000

    fix: fix the console msg on app.listen

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,5 @@ import app from "./app";
 import appConfig from "./config/app.config";
 
 app.listen(appConfig.port, () => {
-  console.log(
-    `[server]: Server is running at http://localhost:${appConfig.port}`
-  );
+  console.log(`[server]: Server is running on ${appConfig.port}`);
 });
